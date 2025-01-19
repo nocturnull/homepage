0e0b648c3d8ea78e1d90a3e4a511596cf9d84a9f
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2025-01-20 02:20:06 +0900

    add work pages

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,17 +5,17 @@
       <li class="navigation-item">
         <a href="#">Work</a>
         <ul class="navigation-sub">
-          <li><a href="#">ONDA</a></li>
-          <li><a href="#">Heartware Korea</a></li>
-          <li><a href="#">Cheritz</a></li>
-          <li><a href="#">KPOP United</a></li>
+          <li class="navigation-item"><NuxtLink to="/work/onda">ONDA</NuxtLink></li>
+          <li class="navigation-item"><NuxtLink to="/work/heartware-korea">Heartware Korea</NuxtLink></li>
+          <li class="navigation-item"><NuxtLink to="/work/cheritz">Cheritz</NuxtLink></li>
+          <li class="navigation-item"><NuxtLink to="/work/kpop-united">KPOP United</NuxtLink></li>
         </ul>
       </li>
       <li class="navigation-item">
         <a href="/">Language</a>
         <ul class="navigation-sub">
-          <li><a href="#">English</a></li>
-          <li><a href="#">한국어</a></li>
+          <li class="navigation-item"><a href="#">English</a></li>
+          <li class="navigation-item"><a href="#">한국어</a></li>
         </ul>
       </li>
     </ul>
---
+++ pages/work/onda.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="work">
+    <div class="work-intro">
+      <h1 class="work-title">ONDA</h1>
+      <p class="work-description">Pig kevin prosciutto short loin pork belly tri-tip drumstick bacon picanha cupim capicola. Rump tenderloin ham, bacon turkey tail landjaeger.</p>
+      <img class="work-poster" src="https://placehold.co/600x400" width="600" alt="Title Image" />
+    </div>
+
+    <div class="work-contents">
+      <section class="container">
+        <h2>Project A</h2>
+        <p>
+          Bacon ipsum dolor amet short loin filet mignon turducken jerky tri-tip beef ribs, kielbasa rump pastrami. Ribeye andouille chicken flank, porchetta kielbasa buffalo t-bone pork meatball capicola swine pig cupim chuck. Andouille ground round ham hock frankfurter beef, jerky rump biltong corned beef. Buffalo turkey filet mignon, kielbasa meatloaf shank landjaeger flank salami. Pork shank ribeye rump kevin.
+        </p>
+        <p>
+          Turducken tail cow leberkas, ribeye burgdoggen pig meatloaf biltong spare ribs ground round sirloin corned beef. Short ribs tail kevin, brisket sausage salami spare ribs sirloin swine leberkas pastrami ham. Doner flank fatback jowl. Hamburger spare ribs pig, rump shankle fatback tenderloin strip steak shank beef. Picanha jowl burgdoggen, landjaeger kielbasa venison turducken tri-tip chuck hamburger chislic andouille fatback. Shoulder filet mignon turkey, pork belly doner prosciutto turducken pork swine beef ground round bresaola.
+        </p>
+      </section>
+    </div>
+  </div>
+</template>
