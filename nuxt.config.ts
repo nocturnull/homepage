c843b1f6ca5531c115f373a8a716545853e9dad3
Marcos Daniel Arroyo <mdarroyo0@gmail.com>
2025-01-21 04:30:15 +0900

    update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ['~/assets/stylesheets/main.scss'],
   modules: ['@nuxtjs/i18n'],
   i18n: {
